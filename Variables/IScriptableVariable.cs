385b7084186dc2d5a40cdaa61b26bfb0ed35f847
Andreas Scherren <abdalamah@gmail.com>
2018-06-30 11:23:59 +0200

    init

---
+++ Variables/IScriptableVariable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace ScriptableSuite.Variables
+{
+	public class IScriptableVariable<T> : ScriptableObject
+	{
+		[NonSerialized] public T Value;
+		[SerializeField] private T _defaultValue;
+
+		private void OnEnable()
+		{
+			Value = _defaultValue;
+		}
+	}
+}
