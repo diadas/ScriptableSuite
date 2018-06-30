fc35a6904c916f3a866bb53f7ee55360273072e0
Andreas Scherren <abdalamah@gmail.com>
2018-06-30 23:09:22 +0200

    AudioClip List

---
+++ Components/TextMeshPro/SetTextByFloatScriptable.cs
@@ -0,0 +1,25 @@
+﻿
+using ScriptableSuite.Variables;
+using TMPro;
+using UnityEngine;
+
+namespace ScriptableSuite.Components.TextMeshPro
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class SetTextByFloatScriptable : MonoBehaviour, IScriptableVariableListener<float>
+    {
+        [SerializeField] private FloatScriptable _floatScriptable;
+        private TextMeshProUGUI _text;
+
+        private void Start()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+            _floatScriptable.Subscribe(this, true);
+        }
+
+        public void OnChange(IScriptableVariable<float> variable)
+        {
+            _text.text = _floatScriptable.Value.ToString("F2");
+        }
+    }
+}
---
+++ Variables/AudioClipListScriptable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableSuite.Variables
+{
+	[CreateAssetMenu(menuName = "ScriptableSuite/Variables/List<AudioClip>")]
+	public class AudioClipListScriptable : IScriptableVariable<List<AudioClip>> {
+	}
+}
